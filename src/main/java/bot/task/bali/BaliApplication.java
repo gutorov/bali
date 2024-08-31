392e5342db125e6b86d7dba6c1533eea154a9914
kirillgutorov <gutorov.k@mail.ru>
2024-08-31 13:43:03 +0700

    initial commit

---
+++ src/main/java/bot/task/bali/BaliApplication.java
@@ -0,0 +1,13 @@
+package bot.task.bali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bot/task/bali/BaliApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.task.bali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
